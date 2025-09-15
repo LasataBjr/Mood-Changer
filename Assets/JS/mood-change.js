abf07021e235bb25134b1937e2bead880d8b897b
LasataBjr <lasatabjrktm@gmail.com>
2025-09-15 18:33:28 +0545

    Gifs on the background

--- Assets/JS/mood-change.js
+++ Assets/JS/mood-change.js
@@ -4,49 +4,49 @@ const moods = [
         emoji: "😄",
         name: "Happy",
         text: "Life feels amazing, keep smiling bro!",
-        color: "#ffe066",
+        bgGif: "Assets/GIFs/happy-walk.gif",
         sound: "Assets/Sounds/happy.mp3"        
     },
     {
         emoji: "😢",
         name: "Sad",
         text: "It's okay to feel down sometimes.",
-        color: "#90CAF9",
+        bgGif: "Assets/GIFs/Sad-walk.gif",
         sound: "Assets/Sounds/sad.mp3"        
     },
     {
         emoji: "😡",
         name: "Angry",
         text: "Take a deep breath... You got this!",
-        color: "#FF8A80",
+        bgGif: "Assets/GIFs/Angry.gif",
         sound: "Assets/Sounds/angry.mp3"        
     },
     {
         emoji: "😌",
         name: "Chill",
         text: "Relax & Chill...This is the moment.",
-        color: "#B2DFDB",
+        bgGif: "Assets/GIFs/Relaxed-Chill.gif",
         sound: "Assets/Sounds/chill.mp3"       
     },
     {
         emoji: "🤩",
         name: "Excited",
         text: "Energy level rising -- Celebrate something small!",
-        color: "#FFD180",
+        bgGif: "Assets/GIFs/Excited-woah.gif",
         sound: "Assets/Sounds/excited.mp3"       
     },
     {
         emoji: "😱",
         name: "Dramatic",
         text: "Why always meeee.... The soap opera of life continues.",
-        color: "#CE93D8",
+        bgGif: "Assets/GIFs/Dramatic.gif",
         sound: "Assets/Sounds/dramatic.mp3"       
     },
     {
         emoji: "😈",
         name: "Mischievous",
         text: "GenZ pranks the whole world -- Serious & Unserious acts.. ",
-        color: "#F48FB1",
+        bgGif: "Assets/GIFs/Mischevious.gif",
         sound: "Assets/Sounds/mischievous.mp3"        
     }
 ];
@@ -55,7 +55,7 @@ const btn = document.getElementById("moodBtn");
 const emoji = document.querySelector(".emoji");
 const moodName = document.getElementById("moodName");
 const moodDesc = document.getElementById("moodDesc");
-const body = document.body;
+// const body = document.body;
 
 let current = 0;
 let audio = new Audio();
@@ -69,8 +69,12 @@ function changeMood(){
     emoji.textContent = mood.emoji;
     moodName.textContent = mood.name;
     moodDesc.textContent = mood.text ;
-    body.style.backgroundColor = mood.color; //Return the background color as document.body.style.backgroundColor
-
+    // body.style.backgroundColor = mood.color; 
+    //Return the background color as document.body.style.backgroundColor
+    // Change background to GIF
+    document.body.style.background = `url('${mood.bgGif}') no-repeat center center fixed`;
+    document.body.style.backgroundSize = "cover";
+    
      //Stop any current playing audio
     audio.pause();
     audio.currentTime = 0;
